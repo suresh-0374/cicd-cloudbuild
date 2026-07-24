eb110d947204d984bee58308a6d0f9833a9fe7a2
suresh-0374 <123469163+suresh-0374@users.noreply.github.com>
2026-07-24 15:39:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return jsonify({
+        "status": "success",
+        "message": "GCP Cloud Build Triggered Successfully!",
+        "framework": "Flask",
+        "language": "Python 3.11"
+    })
+
+if __name__ == "__main__":
+    # GCP Cloud Run/Build uses the PORT environment variable
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
